38b87a2bc303b6d9f0abe7b9429304d8d6b263b9
Shaked Canani <shakedalon5202411@gmail.com>
2026-03-26 23:00:12 +0200

    edit build gradle file

--- FixIt106/build.gradle.kts
+++ FixIt106/build.gradle.kts
@@ -1,6 +1,24 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FixIt106"
+include(":app")
+ 
